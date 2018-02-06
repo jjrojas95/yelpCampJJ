7b832e2919d7c2f071a117075a6458172ead53e5
jjrojas95 <jjrojas95@gmail.com>
2018-02-06 09:54:43 -0500

    init password reset

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 var Campground = require('../models/campground');
-
+var async = require('async');
+var nodemailer = require('nodemailer');
+var crypto = require('crypto');
 //root route
 router.get("/", function(req, res) {
   res.render("landing");
